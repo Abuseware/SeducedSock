e712459e0f4526034547af4cf269b404ce71c98b
Artur Licho Kaleta <licho@protonmail.com>
2016-06-06 05:06:24 +0200

    ~ Divided into two stages ~ Fixed segment registers, causing problems on Bochs and real x86 ~ Updated makefile, readme and gitignore ~ Fixed checks of MBR bootstrap size limit + More properly handling of used registers (forcing known values after boot, etc) + Bochs config - Removed explaination of mysterious bfq

---
+++ stage1.s
@@ -0,0 +1,88 @@
+%include "gpu.inc"
+%include "bochs.inc"
+
+[BITS 16]
+[CPU 586]
+[org 0x7c00]
+
+jmp WORD 0:start								; Unify the segment:offset between BIOSes
+
+start:
+	;; Prepare segments
+	mov ax, 0x2000
+	mov ds, ax
+	mov es, ax
+
+	mov ax, 0x1000
+	mov ss, ax
+	xor sp, sp
+
+	mov ax, (FB / 0x10)
+	mov fs, ax
+	mov gs, ax
+
+	;; Set cursor position to (0,0)
+	push dx 											; Store DX - contains disk id
+	mov dx, 0x3D4
+	mov al, 14
+	out dx, al
+
+	inc dx 												; 0x3D5
+	mov al, 0
+	out dx, al
+
+	dec dx												; 0x3D4
+	mov al, 15
+	out dx, al
+
+	inc dx												; 0x3D5
+	mov al, 0
+	out dx, al
+	pop dx												; Restore DX for later use
+
+clear:
+	;; Clear whole screen
+	xor eax, eax
+	.write:
+		mov [gs:eax], WORD 0x20
+		add eax, 2
+		cmp eax, FB_SIZ
+		jnz .write
+
+loader:
+	;; Read stage2 to ES:BX
+	mov ah, 2											; INT 0x13/0x2 DISK - READ SECTOR(S) INTO MEMORY
+	mov al, 2											; Number of sectors
+	mov ch, 0											; Cylinder
+	mov cl, 2											; Sector
+	mov dh, 0											; Head
+	xor bx, bx
+
+	int 0x13
+
+	cmp ah, 0
+	jz bootstrap
+
+reboot:
+	;; Warm reset if loading stage2 fails
+	mov ax, 0x40
+	mov es, ax
+	mov di, 0x72
+	mov ax, 0x1234
+	stosw
+	jmp WORD 0xFFFF:0x0000
+
+bootstrap:
+	jmp WORD 0x2000:0x0000				; Jump to stage2
+
+fill:
+	;; Check if bootstrap size is less than required 446 bytes
+	%if ($ - $$) > 446
+		%fatal "Bootstrap too fat!"
+	%endif
+
+	;; Fill unused space and partition table
+	times 510 - ($ - $$) db 0
+
+	;; Mark as bootable
+	dw 0xAA55
---
+++ stage2.s
@@ -0,0 +1,79 @@
+%include "gpu.inc"
+%include "bochs.inc"
+
+;; Char color code
+%define LOGO_COLOR 0x02
+
+;; Position of logo
+%define LOGO_X ((FB_W / 2) - 16)
+%define LOGO_Y ((FB_H / 2) - 4)
+%define LOGO_POS (LOGO_X + (FB_W * LOGO_Y)) * 2
+
+[BITS 16]
+[CPU 586]
+[org 0x0000]
+
+start:
+	;; Prepare segments
+	mov ax, 0x2000
+	mov ds, ax
+	mov es, ax
+
+	mov ax, 0x1000
+	mov ss, ax
+	xor sp, sp
+
+	mov ax, (FB / 0x10)
+	mov fs, ax
+	mov gs, ax
+
+logo:
+	;; Draw logo
+	;; Prepare registers for loading string
+	mov edx, LOGO_POS
+	mov ebx, 1
+	mov si, data.str
+
+	.write:
+		;; Read single char from string
+		lodsb
+
+		;; Jump on special chars
+		cmp al, 0xA
+		jz .new_line								;	Make new line on 0xA (\n)
+		cmp al, 0x0
+		jz .end											;	Exit loop on null byte
+
+		;; Write char to framebuffer
+		mov ah, LOGO_COLOR
+		mov [gs:edx], WORD ax
+		add edx, 2
+		jmp .write
+	.new_line:
+		mov edx, FB_W * 2
+		imul edx, ebx
+		add edx, LOGO_POS
+		inc ebx
+		jmp .write
+	.end:
+
+end:
+	;; Loop forever
+	jmp $
+
+data:
+	.str:
+		db '    _/_/_/        _/_/',0xA
+		db '   _/    _/    _/        _/_/_/',0xA
+		db '  _/_/_/    _/_/_/_/  _/    _/',0xA
+		db ' _/    _/    _/      _/    _/',0xA
+		db '_/_/_/      _/        _/_/_/',0xA
+		db '                         _/',0xA
+		db '                        _/',0x0
+
+fill:	
+	;; Fill to 1.5kB
+	%if ($ - $$) > 1535
+		%fatal "Code too fat!"
+	%endif
+	times 1536 - ($ - $$) db 0
