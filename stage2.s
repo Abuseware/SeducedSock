62bf8c0ff13dde15b565114bbc817600ca15f644
Artur Kaleta <49493036+Abuseware@users.noreply.github.com>
2018-03-19 18:59:10 +0100

    Compatibility issues fix and code quality improvement: Changed paging from 1GiB to 2MiB (compatibility issues) Replaced numeric %define with %assing (bug finding) Moved GDT structures to external file (readability) Added "load_segments" macro for quick reload of all segments Removed int 0x15 / 0x3 (AMD prepare for long mode - proper documentation not found) Using new registers in 64b mode. Because, that's why :)

--- stage2.s
+++ stage2.s
@@ -6,12 +6,12 @@
 %include "debug.inc"
 
 ;; Char color code
-%define LOGO_COLOR 0x02
+%assign LOGO_COLOR 0x02
 
 ;; Position of logo
-%define LOGO_X ((FB_W / 2) - 16)
-%define LOGO_Y ((FB_H / 2) - 4)
-%define LOGO_POS (LOGO_X + (FB_W * LOGO_Y)) * 2
+%assign LOGO_X ((FB_W / 2) - 16)
+%assign LOGO_Y ((FB_H / 2) - 4)
+%assign LOGO_POS (LOGO_X + (FB_W * LOGO_Y)) * 2
 
 [CPU x64]
 [BITS 16]
@@ -19,9 +19,13 @@
 start:
 	;; Prepare for protected mode
 	cli
-	lgdt [gdt.gdtr]
+
+	;; Load GDT for protected mode
+	lgdt [gdt32.gdtr]
+
+	;; set protected mode (PE)
 	mov eax, cr0
-	or al, 1 ; set protected mode (PE)
+	or al, 1
 	mov cr0, eax
 
 	DEBUG
@@ -29,31 +33,18 @@ start:
 
 [BITS 32]
 mode32:
-	mov ax, SEG_PROTECT_DS
-	mov ds, ax
-
-	mov ax, SEG_PROTECT_ES
-	mov es, ax
-
-	mov ax, SEG_PROTECT_SS
-	mov ss, ax
-	xor esp, esp
-
-	mov ax, SEG_PROTECT_FS
-	mov fs, ax
-
-	mov ax, SEG_PROTECT_GS
-	mov gs, ax
+	load_segments PROTECT
 
 	;;Prepare paging
 	xor eax, eax
 	mov edi, MEM_PAGETABLE
-	mov ecx, 0x1000
-	cld
+	mov ecx, 0x4000
 	rep stosd
 
 	mov eax, MEM_PAGETABLE
 	mov dword [eax], PT_P | PT_RW | (MEM_PAGETABLE + 0x1000)
+	add eax, 0x1000
+	mov dword [eax], PT_P | PT_RW | (MEM_PAGETABLE + 0x2000)
 	add eax, 0x1000
 	mov dword [eax], PT_P | PT_RW | PT_PS
 
@@ -61,19 +52,23 @@ mode32:
 	mov eax, MEM_PAGETABLE
 	mov cr3, eax
 
+	;; Set PAE
 	mov eax, cr4
-  	or eax, (1 << 5) ; set PAE
+  	or eax, (1 << 5)
   	mov cr4, eax
 
-	mov ecx, 0xC0000080 ; EFER
+  	;; Set EFER
+	mov ecx, 0xC0000080
 	rdmsr
 	or eax, (1 << 8)
 	wrmsr
 
+	;; Set paging (PG)
 	mov eax, cr0
-	or eax, (1 << 31) ; set paging (PG)
+	or eax, (1 << 31)
 	mov cr0, eax
 
+	;; Load GDT for long mode
 	lgdt [gdt64.gdtr]
 	DEBUG
 
@@ -81,28 +76,14 @@ mode32:
 
 [BITS 64]
 mode64:
-	mov ax, SEG_LONG_DS
-	mov ds, ax
-
-	mov ax, SEG_LONG_ES
-	mov es, ax
-
-	mov ax, SEG_LONG_SS
-	mov ss, ax
-	xor esp, esp
-
-	mov ax, SEG_LONG_FS
-	mov fs, ax
-
-	mov ax, SEG_LONG_GS
-	mov gs, ax
+	load_segments LONG
 
 logo:
 	;; Draw logo
 	;; Prepare registers for loading string
-	mov edx, FB + LOGO_POS
-	mov ebx, 1
-	mov esi, data.str
+	mov r8, FB + LOGO_POS
+	mov r9, 1
+	mov rsi, data.str
 
 	.write:
 	;; Read single char from string
@@ -115,15 +96,15 @@ logo:
 
 	;; Write char to framebuffer
 	mov ah, LOGO_COLOR
-	mov [edx], WORD ax
-	add edx, 2
+	mov [r8], WORD ax
+	add r8, 2
 	jmp .write
 
 	.new_line:
-	mov edx, FB_W * 2
-	imul edx, ebx
-	add edx, FB + LOGO_POS
-	inc ebx
+	mov r8, FB_W * 2
+	imul r8, r9
+	add r8, FB + LOGO_POS
+	inc r9
 	jmp .write
 
 	.end:
@@ -137,67 +118,7 @@ end:
 %endif
 
 [segment data start=0x200 vstart=0x20200]
-gdt:
-	.gdtr:
-	istruc gdtr
-	at gdtr.size, dw .size
-	at gdtr.base, dd .gdt
-	iend
-
-	.gdt:
-	.gdt_null:
-	;; Null segment
-	dq 0
-	.gdt_code:
-	;; Code segment
-	istruc gdt_entry
-	at gdt_entry.limit, dw 0xffff
-	at gdt_entry.base, dw 0
-	at gdt_entry.type, dw GDT_TYPE_P | GDT_TYPE_S | GDT_TYPE_CODE_RX
-	at gdt_entry.flags, dw 0xf | GDT_FLAG_G | GDT_FLAG_DB | GDT_FLAG_AVL
-	iend
-
-	.gdt_data:
-	;; Data
-	istruc gdt_entry
-	at gdt_entry.limit, dw 0xffff
-	at gdt_entry.base, dw 0
-	at gdt_entry.type, dw GDT_TYPE_P | GDT_TYPE_S | GDT_TYPE_DATA_RW
-	at gdt_entry.flags, dw 0xf | GDT_FLAG_G | GDT_FLAG_DB | GDT_FLAG_AVL
-	iend
-
-	.size equ $ - .gdt
-
-gdt64:
-	.gdtr:
-	istruc gdtr
-	at gdtr.size, dw .size
-	at gdtr.base, dd .gdt
-	iend
-
-	.gdt:
-	.gdt_null:
-	;; Null segment
-	dq 0
-	.gdt_code:
-	;; Code segment
-	istruc gdt_entry
-	at gdt_entry.limit, dw 0xffff
-	at gdt_entry.base, dw 0
-	at gdt_entry.type, dw GDT_TYPE_P | GDT_TYPE_S | GDT_TYPE_CODE_RX
-	at gdt_entry.flags, dw 0xf | GDT_FLAG_G | GDT_FLAG_L | GDT_FLAG_AVL
-	iend
-
-	.gdt_data:
-	;; Data
-	istruc gdt_entry
-	at gdt_entry.limit, dw 0xffff
-	at gdt_entry.base, dw 0
-	at gdt_entry.type, dw GDT_TYPE_P | GDT_TYPE_S | GDT_TYPE_DATA_RW
-	at gdt_entry.flags, dw 0xf | GDT_FLAG_G | GDT_FLAG_L | GDT_FLAG_AVL
-	iend
-
-	.size equ $ - .gdt
+%include "gdt.s"
 
 data:
 	.str:
