b80b320162c11756be9cdf1821cf734b8706c4c6
Artur Kaleta <hg.licho@mailhero.io>
2018-03-19 20:55:42 +0100

    Build process fix; RLE script now reading in text mode, not binary

--- rle.py
+++ rle.py
@@ -31,9 +31,11 @@ def decode(lst):
 
 
 if __name__ == "__main__":
-    with open(sys.argv[1], 'rb') as rf:
+    with open(sys.argv[1]) as rf:
         with open((sys.argv[1].split('.')[0] + '.s'), 'w') as wf:
             for line in rf:
+                if line == '\n':
+                    continue
                 for x in encode(line):
-                    wf.write("db 0x%X,0x%X\n" % (x[0], x[1]))
+                    wf.write("db 0x%X,0x%X\n" % (ord(x[0]), x[1]))
             wf.write("db 0x0,0x1\n")
