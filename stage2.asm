ba8ab9ba0a524c97582cffbb0093f70eb554ec7c
Artur Kaleta <hg.licho@mailhero.io>
2018-03-26 13:53:00 +0200

    Added interrupt support, fixed stack, disabled cursor.
    Interrupts now are "handled" properly and PIT is used as a system timer with ~0.05s resolution;
    Timer is recalculated to time counter (1s tick);
    Stack in 64b mode is now addressed properly;
    Stage 1 disables cursor in text mode;
    Screen changes foreground color using time counter.

--- stage2.asm
+++ stage2.asm
@@ -2,6 +2,8 @@
 %include "memory.inc"
 %include "gpu.inc"
 %include "gdt.inc"
+%include "idt.inc"
+%include "interrupts.inc"
 %include "paging.inc"
 %include "debug.inc"
 
@@ -17,6 +19,7 @@ start:
 	;; Prepare for protected mode
 	cli
 	load_segments REAL
+	xor sp, sp
 
 	;; Load GDT for protected mode
 	lgdt [gdt32.gdtr]
@@ -26,17 +29,17 @@ start:
 	or al, 1
 	mov cr0, eax
 
-	DEBUG
 	jmp dword SEG_PROTECT_CS:mode32
 
 [BITS 32]
 mode32:
 	load_segments PROTECT
+	mov esp, MEM_PROTECT_STAGE2 - 0x10
 
 	;;Prepare paging
 	xor eax, eax
 	mov edi, MEM_PAGETABLE
-	mov ecx, 0x4000
+	mov ecx, 4096
 	rep stosd
 
 	mov eax, MEM_PAGETABLE
@@ -68,13 +71,95 @@ mode32:
 
 	;; Load GDT for long mode
 	lgdt [gdt64.gdtr]
-	DEBUG
 
 	jmp DWORD SEG_LONG_CS:mode64
 
 [BITS 64]
 mode64:
 	load_segments LONG
+	mov rsp, MEM_PROTECT_STAGE2 - 0x10
+
+	push rsp
+
+	;; Disable NMI
+	in al, 0x70
+	or al, 0x80
+	out 0x70, al
+
+	;; Prepare IDT
+	xor rax, rax
+	mov rdi, MEM_IDT
+	mov rcx, 16 * 256
+	rep stosb
+
+	DEBUG
+	;; Fill IDT
+	mov rcx, 0
+	idt_loop:
+		mov rdx, rcx
+		imul rdx, 16 ; Each entry is 16 bytes wide
+		add rdx, MEM_IDT ; Add starting point
+		mov rax, int_default
+		mov WORD [rdx], ax ; put low 16 bits of addr
+		mov WORD [rdx + 2], SEG_LONG_CS ; selector
+		;mov BYTE [rax + 4], 0; IST
+		mov BYTE [rdx + 5], 0x8e ; entry type and flags
+		shr rax, 0x10 ; get and put middle 16 bits of addr
+		mov WORD [rdx + 6], ax
+		shr rax, 0x10 ; get and put upper 32 bits of addr
+		mov DWORD [rdx + 8], eax
+		;mov DWORD [rax + 12], 0
+
+		inc rcx
+		cmp rcx, 256
+		jnz idt_loop
+
+	idte 0x20, int_0
+
+	DEBUG
+	lidt [idtr64]
+
+	;; Remap PIC
+	mov al, 0x11
+	out 0x20, al ; Restart PIC1
+	out 0xa0, al ; Restart PIC2
+
+	mov al, 0x20 ; PIC1 @32
+	out 0x21, al
+
+	add al, 8    ; PIC2 @40
+	out 0xa1, al
+
+	mov al, 0x4 ; cascade
+	out 0x21, al
+	mov al, 0x2
+	out 0xa1, al
+
+	mov al, 0x1 ; icw4 80x86
+	out 0x21, al
+	out 0xa1, al
+
+	;; Disable PIC interrupts
+	mov al, 0xff
+	out 0x21, al
+	out 0xa1, al
+
+	;; Enable RTC interrupt
+	in al, 0x21
+	and al, ~1 ; PIT
+	out 0x21, al
+
+	;; Configure PIT
+	mov al, (2 << 1) | (3 << 4)
+	out 0x43, al
+	mov ax, 59659; Set clock divider, 1193182Hz/59660 = 0.05000(…garbage…)
+	out 0x40, al
+	shr ax, 8
+	out 0x40, al
+
+
+	DEBUG
+	sti
 
 clear:
 	;; Clear whole screen
@@ -125,6 +210,7 @@ logo:
 	jmp .rle
 
 	.new_line:
+
 	mov r8, FB_W * 2
 	imul r8, r9
 	add r8, FB
@@ -135,15 +221,94 @@ logo:
 	.end:
 
 end:
-	jmp $
+	mov al, [data.counter]
+	;; Convert time counter to color code (1-15)
+	mov bl, 15
+	div bl
+	shr ax, 8
+	add al, 1
 
-%if ($ - $$) > 0x199
+	;; Redraw FB with new color, skipping first char (for cursor hiding)
+	mov rdx, FB+1
+	.loop:
+	mov [rdx], BYTE al
+	add rdx, 2
+	cmp rdx, FB + FB_SIZ + 1
+	jnz .loop
+
+	hlt
+	jmp end
+
+;; Default interrupt handler
+int_default:
+	push rax
+
+	;mov al, 0xa
+	;out 0x20, al
+	;out 0xa0, al
+	;in al, 0xa0
+	;shl al, 8
+	;in al, 0x20
+
+	mov al, 0x20
+    out 0x20, al
+    out 0xa0, al
+    pop rax
+	iretq
+
+;; Handle PIT interrupt (timer)
+int_0:
+	push rax
+	push rbx
+	push rcx
+	push rdx
+
+	xor rax, rax
+
+	;; Keep tick counter running freely
+	mov al, [data.ticks]
+	inc al
+	mov [data.ticks], al
+
+	;; Calculate timer from ticks count
+	mov bl, 20 ; Divide ticks by 20: 0.05 * 20 = 1s :)
+	div bl
+	cmp ah, 0
+	ja .end ; Skip if not divisible
+
+	mov al, [data.counter]
+	inc al
+	mov [data.counter], al
+
+	.end:
+
+	;; Send EOI to PIC
+	mov al,0x20
+    out 0x20,al
+
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    iretq
+
+
+%if ($ - $$) > 0x499
 	%fatal "Stage 2 too big!"
 %endif
 
-[segment data start=0x200 vstart=0x20200]
+[segment data start=0x500 vstart=0x20500]
 %include "gdt.asm"
+
+align 4
+idtr64:
+istruc idtr
+at idtr.limit, dw (256 * 16) - 1
+at idtr.base, dq MEM_IDT
+iend
 
 data:
 	.str:
 		%include "logo.asm"
+	.ticks: db 0
+	.counter: db 0
